bd392ecc0d490a70c9588ff51b16416015995cf7
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2023-05-02 15:51:28 +0200

    changed to test baase64 encoding

--- placeholder-client.py
+++ placeholder-client.py
@@ -1,11 +1,11 @@
 import socket
-
+import base64
 HOST = "localhost"  # The server's hostname or IP address
-PORT = 42750  # The port used by the server
-
+PORT = 42751  # The port used by the server
+ENCODING = "ascii"
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"OneRing.txt")
-    data = s.recv(1024)
+    s.sendall(base64.encodebytes("GET subfolder1 PTTP/1.0".encode(ENCODING)))
+    data = base64.decodebytes(s.recv(1024)).decode(ENCODING)
 
-print(f"Received {data!r}")
+print(f"Received: {data}")
